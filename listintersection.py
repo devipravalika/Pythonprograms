6018c68f74c7406e3d5e4397d36e923c84a3059d
devipravalika <119585638+devipravalika@users.noreply.github.com>
2022-12-01 02:40:50 -0800

    Update listintersection.py

--- listintersection.py
+++ listintersection.py
@@ -1,6 +1,7 @@
 color_list1=set(["white","Black","Red"])
 color_list2=set(["Red","Green"])
-#for ele in color_list1:
-    #if color_list1[ele]==color_list2[ele]:
-        #color_list3+=colorlist1[ele]
-print(color_list1.difference(color_list2))
+color_list3=[]
+for ele in color_list1:
+    if ele not in color_list2:
+        color_list3.append(ele)
+print(color_list3)
