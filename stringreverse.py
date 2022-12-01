7c886611278806c70139cb144f54f91ec7ade5ba
devipravalika <119585638+devipravalika@users.noreply.github.com>
2022-12-01 02:53:40 -0800

    Add files via upload

--- stringreverse.py
+++ stringreverse.py
@@ -1,8 +1,8 @@
 def reverse(a):
-   print(a[::-1])
+   return a[::-1]
 
 first_name=input("enter your firstname :")
 last_name=input("enter your lastname :")
-reverse(first_name)
-reverse(last_name)
+print(reverse(first_name)+" "+reverse(last_name))
+
 
